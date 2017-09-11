c7e43c5a20e21116f4b8505f9657e3561706858c
Odigietony <odigietony.jr@gmail.com>
2017-09-11 16:03:55 +0100

    a work in progress

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LogBookWebApp.Startup))]
+namespace LogBookWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
